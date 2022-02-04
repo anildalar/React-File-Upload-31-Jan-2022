82422049f59a0790dfc0532fc48d124711f8fa63
“anildalar” <dollorinfotech@gmail.com>
2022-02-04 09:50:40 +0530

    v4

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={ <Login2 /> } />
-          <Route path="/" element={ <FileUpload /> } />
+          <Route path="/upload" element={ <FileUpload /> } />
           <Route path="*" element={ <Error404 /> } />
         </Routes>
       </Router>
--- src/Pages/Login2.js
+++ src/Pages/Login2.js
@@ -12,7 +12,7 @@ export default function Login2() {
         identifier:'anil@gmail.com',
         password:'anil@123'
     }) // {P:V,P:V} = JS Object
-    const [user,setUser] = useState({
+    const [user,setUser] = useState({ //JS Object   object.property
         user:null,
         is_loggedin:true
     })
@@ -39,10 +39,19 @@ export default function Login2() {
             
         }
 
-        alert('Page Loaded successfully!');
+        //alert('Page Loaded successfully!');
     },[])
 
     //2. Functions defination
+    let logout = ()=>{
+        console.log('OKOKOKOK');
+        //now remove the local storage value
+        localStorage.removeItem('user');
+        //localStorage.clear();
+        //Pure JS Technique
+
+        window.location.replace('/')
+    }
     let handleChange = (e)=>{ // e = event e is a formal argument
         console.log(e.target.classList.contains('a_username'));
         if(e.target.classList.contains('a_username')){
@@ -73,6 +82,7 @@ export default function Login2() {
         console.log("ok")
 
         try {
+            // await wait for PO
             let {data}= await axios.post(`${config2.dev_api_url}/api/auth/local`, {
                 identifier: data2.identifier,
                 password:data2.password ,
@@ -100,23 +110,32 @@ export default function Login2() {
         <>
             <div className="row">
                 <div className="col-6 offset-3 pt-5">
-                    <h1 className="text-center">Login Form</h1>
-                    <form onSubmit={ (e)=>{ login(e) } }>
-                        <div className="mb-3">
-                            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                            <input type="email" className="form-control a_username" name="identifier" onChange={(e)=>{ handleChange(e) }} id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                            <input type="password" name="password"  onChange={(e)=>{ handleChange(e) }} className="form-control a_password" id="exampleInputPassword1" />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
+                { user.is_loggedin > 0 ||
+                    <>
+                        <h1 className="text-center">Login Form</h1>
+                        <form onSubmit={ (e)=>{ login(e) } }>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                                <input type="email" className="form-control a_username" name="identifier" onChange={(e)=>{ handleChange(e) }} id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                                <input type="password" name="password"  onChange={(e)=>{ handleChange(e) }} className="form-control a_password" id="exampleInputPassword1" />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>    
+                    </>
+                    
+                }
+                    
                 </div>
             </div>
             { user.is_loggedin &&
-                <FileUpload2 />
+                <>
+                    <button onClick={ ()=>{ logout() }} className="btn btn-success text-center">Logout</button>
+                    <FileUpload2 />
+                </> 
             }
             
             
